b59788417539bf9ae032de8044932514d7a348ac
Ramy-Gendy <61162010+Ramy-Gendy@users.noreply.github.com>
2020-11-10 22:48:04 +0400

    Added Registeration Validation

--- app/src/main/java/com/example/mycoronatracker/LoginActivity.java
+++ app/src/main/java/com/example/mycoronatracker/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.example.mycoronatracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     private EditText loginEmailET;
     private EditText loginPassET;
@@ -30,23 +41,89 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         loginButton = (Button) findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
 
 
-    private void validateLoginDetails() {
-
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            return;
+        }
     }
+
+//    private void validateLoginDetails() {
+//        String error = "";
+//        if (registerEmailET == null || registerPassET == null || registerConfirmPassET == null) {
+//            error = "Please fill all fields.";
+//            registerEmailTV.setText(error);
+//            registerPassTV.setText(error);
+//            registerConfirmPassTV.setText(error);
+//            return false;
+//        }
+//        else if (!isValidEmail(registerEmailET.getText().toString())) {
+//            error = "Please enter a valid email.";
+//            registerEmailTV.setText(error);
+//            return false;
+//        }
+//        // TODO: Implement preexisting email checking
+//        else if (registerPassET.getText().toString().length() < 8) {
+//            error = "The password must be longer than 8 characters.";
+//            registerPassTV.setText(error);
+//            return false;
+//        }
+//        else if (!registerPassET.getText().toString().equals(registerConfirmPassET.getText().toString())) {
+//            error = "Both passwords must match.";
+//            registerPassTV.setText(error);
+//            registerConfirmPassTV.setText(error);
+//            return false;
+//        }
+//        return true;
+//
+//    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginButton:
-                validateLoginDetails();
-                Intent homeActivity = new Intent(this, HomeActivity.class);
-                startActivity(homeActivity);
+
+                String email = loginEmailET.getText().toString();
+                String password = loginPassET.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("SUCCESS", "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("FAIL", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                    // ...
+                                }
+                                // ...
+                            }
+                        });
                 break;
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            String email = loginEmailET.getText().toString();
+            Intent homeActivity = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(homeActivity);
         }
     }
 }
--- app/src/main/java/com/example/mycoronatracker/RegisterActivity.java
+++ app/src/main/java/com/example/mycoronatracker/RegisterActivity.java
@@ -1,17 +1,32 @@
 package com.example.mycoronatracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity implements OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     private EditText registerEmailET;
     private EditText registerPassET;
@@ -38,6 +53,8 @@ public class RegisterActivity extends AppCompatActivity implements OnClickListen
 
         registerButton.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
 
@@ -45,13 +62,75 @@ public class RegisterActivity extends AppCompatActivity implements OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.registerButton:
-                validateRegisterDetails();
-                Intent loginActivity = new Intent(this, LoginActivity.class);
-                startActivity(loginActivity);
+                if (!validateRegisterDetails()) {
+                    break;
+                }
+
+                String email = registerEmailET.getText().toString();
+                String password = registerPassET.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("SUCCESS", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("FAIL", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+
+                            private void updateUI(FirebaseUser user) {
+                                if (user != null) {
+                                    String userEmail = user.getEmail().toString();
+                                    Intent homeActivity = new Intent(RegisterActivity.this, HomeActivity.class);
+                                    homeActivity.putExtra("email", userEmail);
+                                    startActivity(homeActivity);
+                                }
+                            }
+
+                        });
                 break;
         }
     }
 
-    private void validateRegisterDetails() {
+    private boolean validateRegisterDetails() {
+        String error = "";
+        if (registerEmailET == null || registerPassET == null || registerConfirmPassET == null) {
+            error = "Please fill all fields.";
+            registerEmailTV.setText(error);
+            registerPassTV.setText(error);
+            registerConfirmPassTV.setText(error);
+            return false;
+        }
+        else if (!isValidEmail(registerEmailET.getText().toString())) {
+            error = "Please enter a valid email.";
+            registerEmailTV.setText(error);
+            return false;
+        }
+        // TODO: Implement preexisting email checking
+        else if (registerPassET.getText().toString().length() < 8) {
+            error = "The password must be longer than 8 characters.";
+            registerPassTV.setText(error);
+            return false;
+        }
+        else if (!registerPassET.getText().toString().equals(registerConfirmPassET.getText().toString())) {
+            error = "Both passwords must match.";
+            registerPassTV.setText(error);
+            registerConfirmPassTV.setText(error);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
